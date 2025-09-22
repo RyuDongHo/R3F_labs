ee1f65318df033b9f9b773c5415d86c8653903f5
unknown <thffhahs03@gmail.com>
2025-09-22 23:02:10 +0900

    chore: design

--- src/Page/SphereTest/index.tsx
+++ src/Page/SphereTest/index.tsx
@@ -4,14 +4,17 @@ import Sphere from "./ui/Sphere";
 const SphereTest = () => {
   return (
     <main className=" w-full h-full flex flex-col items-center pt-8">
-      <div className=" flex w-full p-[32px] items-center gap-[64px]">
-        <div className=" flex justify-between w-[30%] items-center">
-          <h1 className=" h-[64px] text-3xl font-bold text-center my-4">
+      <div className=" flex w-full p-[24px] items-center gap-[64px]">
+        {/* Title */}
+        <div className=" flex justify-between xl:w-[30%] w-full items-center">
+          <h1 className=" xl:h-[64px] xl:text-3xl text-xl font-bold text-center my-4">
             Creative Labs.
           </h1>
           <div className=" flex flex-col">
-            <strong className=" text-2xl">Make</strong>
-            <strong>Creative Things.</strong>
+            <strong className=" xl:text-2xl text-sm">Make</strong>
+            <strong className=" xl:text-[16px] text-xs">
+              Creative Things.
+            </strong>
           </div>
         </div>
       </div>
