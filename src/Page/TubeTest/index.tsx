2288ea037789ea12525d135e44b9dcb5c3b74ef2
unknown <thffhahs03@gmail.com>
2025-09-29 04:29:02 +0900

    feat: tube test tube test 초기 layout 추가

--- src/Page/TubeTest/index.tsx
+++ src/Page/TubeTest/index.tsx
@@ -1,19 +1,18 @@
 import { CameraControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
+import { Text, Billboard } from "@react-three/drei";
 
 const TubeTest = (): React.ReactElement => {
-
   const points: THREE.Vector3[] = [];
-  const count = 360;
-  const amplitude = 10;
-  const division = 15;
-
-  for(let i = 0; i < count; i++) {
-    const x =  i / division;
+  const count = 720;
+  const amplitude = 3;
+  const division = 50;
+  const curveGroupPosition = new THREE.Vector3(-(count / division) / 2, 0, 0);
+  for (let i = 0; i < count; i += 3) {
     const sin = Math.sin(THREE.MathUtils.degToRad(i)) * amplitude;
-    //const cos = Math.cos(THREE.MathUtils.degToRad(i)) * amplitude;
-    points.push(new THREE.Vector3(x - (count / division / 2), sin, 0));
+    const cos = Math.cos(THREE.MathUtils.degToRad(i)) * amplitude;
+    points.push(new THREE.Vector3(i / division, sin, cos));
   }
   return (
     <>
@@ -29,14 +28,15 @@ const TubeTest = (): React.ReactElement => {
         <directionalLight position={[5, 5, 5]} intensity={1} />
         <ambientLight intensity={0.5} />
         <gridHelper args={[10, 10, `white`, `gray`]} position={[0, 0, 0]} />
-        {
-          points.map((point, index) => (
-            <mesh key={index} position={point}>
-              <sphereGeometry args={[0.1, 32, 32]} />
-              <meshStandardMaterial color="orange" />
-            </mesh>
-          ))
-        }
+        <group position={curveGroupPosition}>
+          {points.map((point, index) => (
+            <Billboard key={index} position={point}>
+              <Text color={"red"} fontSize={0.3}>
+                {index}
+              </Text>
+            </Billboard>
+          ))}
+        </group>
       </Canvas>
     </>
   );
